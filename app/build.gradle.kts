2ff07da1eae52ffc41ebc887e41b49f19f9e5f6c
Ming Ip <ming.ip@paywiser.com>
2025-06-03 21:59:41 +0100

    Fixed RTMP not playing problem;

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.mmhw.csvtv"
         minSdk = 21
         targetSdk = 35
-        versionCode = 5
-        versionName = "1.05"
+        versionCode = 6
+        versionName = "1.06"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
